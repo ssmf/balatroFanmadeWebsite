73fa136174527ac9e7cd4953efbbb514a6a93b31
ssmf <chamberkid1@gmail.com>
2024-03-03 23:26:16 +0100

    initialized repo

---
+++ src/App.vue
@@ -0,0 +1,127 @@
+<script setup>
+    import MainButton from './components/Button.vue';
+    import { ref } from 'vue';
+
+    const trueBool = ref(true);
+</script>
+
+<template>
+  <div class="backgroundImage"></div>
+  <div class="main">
+    <img src="./assets/images/mainLogo.png" class="mainLogo">
+    <h1 class="availableHeader">AVAILABLE NOW!</h1>
+    <div class="buttonWrapper">
+        <MainButton :ifImage="false" buttonContent="NEWSLETTER"/>
+        <MainButton :ifImage="false" buttonContent="WHAT IS BALATRO"/>
+        <MainButton :ifImage="false" buttonContent="TRAILER"/>
+    </div>
+    <div class="StoresWrapper">
+        <a href="https://www.nintendo.com/us/store/products/balatro-switch/" target="_blank" class="storeLink"><img src="./assets/images/NintendoLogo.png" class="storeLogo"></a>
+        <a href="https://store.playstation.com/en-pl/concept/10010334/" target="_blank" class="storeLink"><img src="./assets/images/PlaystationLogo.png" class="storeLogo"></a>
+        <a href="https://www.xbox.com/en-CA/games/store/balatro/9pk087lngjc5" target="_blank" class="storeLink"><img src="./assets/images/XboxLogo.png" class="storeLogo"></a>
+        <a href="https://store.steampowered.com/app/2379780/Balatro/?utm_source=website" target="_blank" class="storeLink"><img src="./assets/images/SteamLogo.png" class="storeLogo"></a>
+    </div>
+    <div class="othersWrapper">
+        <MainButton :ifImage="true" buttonContent='LocalThunkLogo.png'/>
+        <MainButton :ifImage="true" hoverColor="25deg" buttonContent='DiscordLogo.svg'/>
+        <MainButton :ifImage="true" buttonContent="PlaystackLogo.png"/>
+        <MainButton :ifImage="false" buttonContent="Press Pack!" style="position: relative;"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+
+
+    .backgroundImage {
+        background-image: url("@/assets/Images/mainBackground.jpeg");
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        z-index: -1;
+    }
+
+    .main {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        height: 100%;
+        max-width: 100%;
+        flex-direction: column;
+        gap: 50px;
+    }
+
+    .mainLogo {
+        width: 40%;
+        min-width: 400px;
+        margin-top: 30px;
+        transition: all .15s ease-in-out;
+    }
+
+    .availableHeader {
+        margin: 0px;
+        margin-top: -20px;
+        color: var(--mainRed);
+        font-size: 40px;
+        text-shadow: 5px 5px 1px black;
+        transition: .15s all ease-in-out;
+    }
+
+    .availableHeader:hover {
+        text-shadow: -5px 5px 1px black;
+    }
+
+    .buttonWrapper {
+        position: relative;
+        margin-top: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+        flex-wrap: wrap;
+        gap: 25px;
+    }
+
+    .StoresWrapper {
+        background-color: rgba(0, 0, 0, 0.5);
+        border-radius: 25px;
+        padding: 20px 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 30px;
+        box-shadow: 0px 0px 10px 1px black;
+        transition: .15s all ease-in-out;
+    }
+
+    .StoresWrapper:hover {
+        box-shadow: 0px 0px 15px 5px black;
+    }
+
+    .storeLogo {
+        width: 100%;
+        transition: all .15s ease-in-out;
+    }
+
+    .storeLogo:hover {
+        filter:drop-shadow(0px 0px 10px white);
+        cursor: pointer;
+    }
+
+    .storeLink {
+        width: 100%;
+    }
+
+    .othersWrapper {
+        display: flex;
+        justify-content: center;
+        align-items: stretch;
+        gap: 20px;
+        flex-wrap: wrap;
+        width: 400px;
+    }
+
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,65 @@
+<script setup>
+    import { ref } from 'vue';
+    
+    const props = defineProps({
+        buttonContent: String,
+        hoverColor: {
+            type: String,
+            default: '140deg'
+        }
+    });
+
+    const ifImage = defineModel('ifImage');
+    const imagePath = 'src/assets/images/' + props.buttonContent;
+
+    console.log(props.hoverColor)
+</script>
+
+<template>
+
+<div v-if="!ifImage" class="mainButton">
+    {{ buttonContent }}
+</div>
+
+<div v-else class="mainButton">
+    <img class="buttonImage" :src="imagePath">
+</div>
+
+
+    
+</template>
+
+<style scoped>
+    .mainButton {
+        position: inherit;
+        top: 0px;
+        padding: 15px 30px;
+        font-size: 25px;
+        background-image: url('../assets/Images/newsletterButton.png');
+        background-repeat: repeat;
+        background-size: 100%;
+        border-radius: 15px;
+        color: var(--lightBlue);
+        border-bottom: 4px solid var(--darkBlue);
+        outline: 1px solid black;
+        box-shadow: 0px 6px 15px 8px var(--darkBlue);
+        transition: all .15s ease-in-out;
+        user-select: none;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        
+    }
+
+    .mainButton:hover {
+        top: 10px;
+        box-shadow: 0px 0px 15px 10px var(--darkBlue);
+        cursor: pointer;
+        filter: hue-rotate(v-bind(hoverColor));
+
+    }
+
+    .buttonImage {
+        width: 50px;
+    }
+</style>
