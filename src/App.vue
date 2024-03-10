a24edc1b315c219b097899372c9467dfee817f24
ssmf <chamberkid1@gmail.com>
2024-03-10 18:28:09 +0100

    Attempt on fixing background resizing issues on mobile

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@
         background-repeat: no-repeat;
         background-position: center;
         background-size: cover;
-        width: 100vw;
+        width: 100%;
         height: 100%;
         position: fixed;
         z-index: -1;
